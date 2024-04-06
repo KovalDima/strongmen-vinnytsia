832836c1c8cb54eb2e3af687f793eae86e08f650
Dima <mr.smith8263@gmail.com>
2024-04-06 18:55:34 +0200

    added questions and footer

--- js/index.js
+++ js/index.js
@@ -8,7 +8,31 @@ const testimonialsSlider = new Swiper(".testimonials__slider", {
     clickable: true,
   },
   autoplay: {
-    delay: 5000,
+    delay: 6000,
     pauseOnMouseEnter: true,
   },
 });
+
+accordion(".questions__item-head");
+
+function accordion(className) {
+  let accBtn = document.querySelectorAll(className);
+
+  for (let i = 0; i < accBtn.length; i++) {
+    accBtn[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      let accPanel = this.nextElementSibling;
+      if (accPanel.style.maxHeight) {
+        accPanel.style.maxHeight = null;
+        accPanel.style.marginBottom = null;
+      } else {
+        accPanel.style.maxHeight = `${accPanel.scrollHeight}px`;
+        accPanel.style.marginBottom = "30px";
+      }
+    });
+  }
+}
+
+// auto copyrigth year
+const year = document.querySelector(".footer__copy span");
+year.textContent = new Date().getFullYear();
